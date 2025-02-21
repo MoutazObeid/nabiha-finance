0d13763da8a76da33bede7d00cfeb4a1ebd5c04a
MoutazObeid <moutazobeid24@gmail.com>
2025-02-21 20:42:14 +0200

    Added total amount of expenses

--- nabiha.py
+++ nabiha.py
@@ -3,10 +3,17 @@
 
 Salary = int(input("Please enter Nabiha's salary: "))
 Month = input("Please enter the month of Nabih's salary: ")
-Psaving = int(input("Please enter the saving percentage:"))
+Psavings = int(input("Please enter the saving percentage:"))
 Prent = int(input("Please enter the saving percentage:"))
 Pelectricity = int(input("Please enter the saving percentage:"))
 
-Saving = (Salary*Psaving)/100
+Savings = (Salary*Psavings)/100
 Rent = (Salary*Prent)/100
 Electricity = (Salary*Pelectricity)/100
+
+print("The amount of savings is: "+str(Savings))
+print("The amount of rent is: "+str(Rent))
+print("The amount of savings is: "+str(Electricity))
+
+Expenses = Savings + Rent + Electricity
+print("The total amount of the expenses: "+str(Expenses))
