a3e0cf84d00782b038c9a11a549c17af2cf9a184
MoutazObeid <moutazobeid24@gmail.com>
2025-02-21 20:32:21 +0200

    Amount calculated for savings,rent and electricity

---
+++ nabiha.py
@@ -0,0 +1,12 @@
+
+
+
+Salary = int(input("Please enter Nabiha's salary: "))
+Month = input("Please enter the month of Nabih's salary: ")
+Psaving = int(input("Please enter the saving percentage:"))
+Prent = int(input("Please enter the saving percentage:"))
+Pelectricity = int(input("Please enter the saving percentage:"))
+
+Saving = (Salary*Psaving)/100
+Rent = (Salary*Prent)/100
+Electricity = (Salary*Pelectricity)/100
